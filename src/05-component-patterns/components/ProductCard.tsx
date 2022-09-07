7e90e7298e201043b94495ccc0cda872018a264e
Katherine <katherine.provostelara@gmail.com>
2022-09-07 16:44:35 -0300

    Component Extend Styles Listo

---
+++ src/05-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+//import { useProduct } from "../hooks/useProduct";
+import styles from "../styles/styles.module.css";
+import { createContext, CSSProperties, ReactElement } from "react";
+import { useProduct } from "../hooks/useProduct";
+import { Product, ProductContextProps } from "../interfaces/interfaces";
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+export interface Props {
+  product: Product;
+  children?: ReactElement | ReactElement[];
+  className?: string;
+  style?: CSSProperties;
+}
+
+export const ProductCard = ({ children, product, className, style }: Props) => {
+  const { counter, increaseBy } = useProduct();
+
+  return (
+    <Provider value={{ counter, increaseBy, product }}>
+      <div className={`${styles.productCard} ${className}`} style={style}>
+        {children}
+      </div>
+    </Provider>
+  );
+};
---
+++ src/05-component-patterns/interfaces/interfaces.ts
@@ -0,0 +1,23 @@
+import { Props as ProductCardProps } from "../components/ProductCard";
+import { Props as ProductButtonsProps } from "../components/ProductButtons";
+import { Props as ProductImageProps } from "../components/ProductImage";
+import { Props as ProductTitleProps } from "../components/ProductTitle";
+
+export interface Product {
+  id: string;
+  title: string;
+  img?: string;
+}
+
+export interface ProductContextProps {
+  counter: number;
+  increaseBy: (value: number) => void;
+  product: Product;
+}
+
+export interface ProductCardHOKProps {
+  ({ children, product }: ProductCardProps): JSX.Element;
+  Buttons: (Props: ProductButtonsProps) => JSX.Element;
+  Image: (Props: ProductImageProps) => JSX.Element;
+  Title: (Props: ProductTitleProps) => JSX.Element;
+}
